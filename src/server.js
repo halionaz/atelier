fe73da5fa44c36b2e863e72a6e75e9d57e4740f2
haSaJa <samhsj@naver.com>
2021-09-03 00:45:58 +0900

    express setting

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
 console.log("Hello");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req,res) => {
+    fs.readFile("./src/views/index.html",(err,data) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.write(data);
+        }
+    })
+})
+
+app.listen(3000);
