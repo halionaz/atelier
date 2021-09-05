0d50c88662824f2a78d0ba2cedefd437f5962d2c
haSaJa <samhsj@naver.com>
2021-09-05 09:21:30 +0900

    랜덤 짹짹입숨

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,2 +1,34 @@
+const lipsum = document.querySelector("#lipsum");
+const nameH1 = lipsum.querySelector("h1");
 
+function randomLipsum(){
+    const count = Math.floor(Math.random() * 2) + 2;
+    let capCount, wordCount, dice;
+    for(let i = 0; i < count; i++){
+        const div = document.createElement("div");
+        const caption = document.createElement("div");
+        capCount = Math.floor(Math.random() * 2) + 2;
+        let text = "";
+        for(let j = 0; j < capCount; j++){
+            wordCount = Math.floor(Math.random() * 3) + 1;
+            for(let l = 0; l < wordCount; l++){
+                text += "짹";
+            }
+            dice = Math.floor(Math.random()*6);
+            if(dice < 2){
+                text += "..";
+            } else {
+                if(j != capCount - 1){
+                    text += " ";
+                }
+            }
+        }
+        caption.classList.add("caption");
+        caption.innerText = text;
+        div.appendChild(caption);
+        nameH1.after(div);
+    }
+}
+
+randomLipsum();
 console.log("발견된 문제가 없습니다.");
