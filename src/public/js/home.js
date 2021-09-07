679fb490df426fe0ee9f1d88c741a7e38afa77b8
haSaJa <samhsj@naver.com>
2021-09-08 02:05:43 +0900

    HTML 업데이트

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,5 +1,7 @@
 const lipsum = document.querySelector("#lipsum");
 const nameH1 = lipsum.querySelector("h1");
+const timePercent = lipsum.querySelector("#timePercent");
+const timeDay = lipsum.querySelector("#timeDay");
 
 /**
  * 짹짹으로 이루어진 랜덤 로렘 입숨을 만들어주는 함수입니다.
@@ -35,5 +37,22 @@ function randomLipsum(){
     }
 }
 
+function dday(){
+
+    const startDate = new Date("2021-09-01T00:00:00");
+    const setDate = new Date("2022-01-14T08:00:00");
+    const now = new Date();
+
+    const all = setDate.getTime() - startDate.getTime();
+    const dist = setDate.getTime() - now.getTime();
+    const percent = (((all-dist)/all)*100).toFixed(2);
+    const day = Math.floor(dist/(1000*60*60*24));
+    timePercent.innerText = percent;
+    timeDay.innerText = day
+    dateWhenIGoToAirForce-date
+
+}
+
 randomLipsum();
+dday();
 console.log("발견된 문제가 없습니다.");
