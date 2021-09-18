869034f081669384f3d2338c0cd6dd800d9295dd
haSaJa <samhsj@naver.com>
2021-09-18 21:14:56 +0900

    세션을 통한 사용자 구분 구현 및 gitignore 업데이트

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import favicon from "serve-favicon";
+import session from "express-session";
+import {sec} from "./secret.js";
+const FileStore = require('session-file-store')(session);
+
+let memberCnt = 1;
 
 const app = express();
 
@@ -10,8 +15,22 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.use(favicon(__dirname + '/public/icon/magpie.ico'));
 
+app.use(session({
+    HttpOnly: true,
+    secret: sec,
+    resave: false,
+    saveUninitialized: true,
+    store : new FileStore({logFn: function(){}})
+}));
+
 app.get("/", (req,res) => {
-    console.log("서버에 접속했습니다.");
+    if(req.session.num){
+        console.log(`${req.session.num}가 서버에 접속했습니다.`);
+    } else {
+        req.session.num = memberCnt;
+        memberCnt++;
+        console.log(`새로운 참가자, ${req.session.num}이 참가했습니다.`);
+    }
     res.render("home");
 });
 
