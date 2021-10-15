0302eba8a72481789147c61ef4aa9fa3b403c918
haSaJa <samhsj@naver.com>
2021-10-15 21:38:04 +0900

    ply.js 주석 처리

--- src/public/js/ply.js
+++ src/public/js/ply.js
@@ -1,7 +1,4 @@
-console.log("Hello, It's working");
-
-// Designed by:  Mauricio Bucardo
-// Original image: https://dribbble.com/shots/6957353-Music-Player-Widget
+// Original Designed by:  Mauricio Bucardo
 
 "use strict";
 
@@ -24,68 +21,75 @@ const sliderContent = slider.querySelector(".slider__content");
 const sliderContentLength = playerPlayList.length - 1;
 const sliderWidth = 100;
 let left = 0;
-let count = 0;
+let count = 0; // 현재 선택된 노래 index
 let song = playerSongs[count];
-let isPlay = false;
+let isPlay = false; // 재생 여부 체크
 const pauseIcon = playButton.querySelector("img[alt = 'pause-icon']");
 const playIcon = playButton.querySelector("img[alt = 'play-icon']");
 const progres = player.querySelector(".progres");
 const progresFilled = progres.querySelector(".progres__filled");
 let isMove = false;
 
-// creat functions
+// 노래 한곡 집중 모드 ON
 function openPlayer() {
-
+    
     playerHeader.classList.add("open-header");
     playerControls.classList.add("move");
     slider.classList.add("open-slider");
     
 }
 
+// 노래 한곡 집중 모드 OFF
 function closePlayer() {
-
+    
     playerHeader.classList.remove("open-header");
     playerControls.classList.remove("move");
     slider.classList.remove("open-slider");
     
 }
 
+// 다음 곡으로 전환
 function next(index) {
     
-    count = index || count;
-
+    count = index || count; // 플레이리스트 선곡을 하면 index에 값이 들어옴, 그렇지 않으면 단순 count ++
+    
     if (count == sliderContentLength) {
+        // 끝 곡이므로 아무 일도 일어나지 않음
         count = count;
         return;
     }
-
+    
+    // 앨범 커버 오른쪽으로 밀기
     left = (count + 1) * sliderWidth;
     left = Math.min(left, (sliderContentLength) * sliderWidth);
     sliderContent.style.transform = `translate3d(-${left}%, 0, 0)`;
-    count++;
+    count++; // 곡 인덱스 변경
     run();
-
+    
 }
 
+// 이전 곡으로 전환
 function back(index) {
     
     count = index || count;
-
+    
     if (count == 0) {
         count = count;
         return;
     }
     
+    // 앨범 커버 왼쪽으로 밀기
     left = (count - 1) * sliderWidth;
     left = Math.max(0, left);
     sliderContent.style.transform = `translate3d(-${left}%, 0, 0)`;
     count--;
     run();
-
+    
 }
 
+// 음악 플레이어 위에 얹혀진 노래 정보 변경
 function changeSliderContext() {
-
+    
     sliderContext.style.animationName = "opacity";
     
     sliderName.textContent = playerPlayList[count].querySelector(".player__title").textContent;
@@ -99,7 +103,7 @@ function changeSliderContext() {
         sliderName.append(textWrap);
         
     }
-
+    
     if (sliderTitle.textContent.length >= 18) {
         const textWrap = document.createElement("span");
         textWrap.className = "text-wrap";
@@ -107,58 +111,64 @@ function changeSliderContext() {
         sliderTitle.innerHTML = "";
         sliderTitle.append(textWrap);
     }
-
+    
 }
 
+// 뒷 배경 색 변경
 function changeBgBody() {
     body.style.backgroundColor = bgBody[count];
 }
 
+// 실제로 재생되는 음원 변경
 function selectSong() {
-
+    
     song = playerSongs[count];
-
+    
     for (const item of playerSongs) {
-
+        
+        //현재 재생곡이 아닌 곡들 모두 pause, 재생 내역 초기화
         if (item != song) {
             item.pause();
             item.currentTime = 0;
         }
-
+        
     }
-
+    
     if (isPlay) song.play();
     
     
 }
 
+// 변경점 생겼을 때 노래 재생
 function run() {
-  
+    
     changeSliderContext();
     changeBgBody();
     selectSong();
-  
+    
 }
 
+// play, pause 버튼 눌렀을 때 쿼리 수행
 function playSong() {
-
+    
     if (song.paused) {
         song.play();
         playIcon.style.display = "none";
         pauseIcon.style.display = "block";
-
+        
     }else{
         song.pause();
         isPlay = false;
         playIcon.style.display = "";
         pauseIcon.style.display = "";
     }
-
-
+    
+    
 }
 
+// 곡이 재생되는 동안 계속해서 progress바를 업데이트 함
 function progresUpdate() {
-
+    
     const progresFilledWidth = (this.currentTime / this.duration) * 100 + "%";
     progresFilled.style.width = progresFilledWidth;
 
@@ -172,14 +182,15 @@ function progresUpdate() {
     }
 }
 
+// 재생 바 위에서의 조정
 function scurb(e) {
-
     // If we use e.offsetX, we have trouble setting the song time, when the mousemove is running
     const currentTime = ( (e.clientX - progres.getBoundingClientRect().left) / progres.offsetWidth ) * song.duration;
     song.currentTime = currentTime;
 
 }
 
+// 곡이 로드되면, 곡의 길이를 받아 표시함
 function durationSongs() {
 
     let min = parseInt(this.duration / 60);
@@ -196,7 +207,7 @@ function durationSongs() {
 
 changeSliderContext();
 
-// add events
+// 이벤트 등록
 sliderContext.addEventListener("click", openPlayer);
 sliderContext.addEventListener("animationend", () => sliderContext.style.animationName ='');
 playlistButton.addEventListener("click", closePlayer);
@@ -220,11 +231,13 @@ playerSongs.forEach(song => {
     
 });
 
+// 프로그래스 바에서 클릭했을 때
 progres.addEventListener("pointerdown", (e) => {
     scurb(e);
     isMove = true;
 });
 
+// 프로그래스 바에서 움직일 때 (클릭한 이후엔 플레이어가 자유롭게 마우스를 움직일 수 있으므로 document에 달아놓음)
 document.addEventListener("pointermove", (e) => {
     if (isMove) {
         scurb(e); 
@@ -232,11 +245,13 @@ document.addEventListener("pointermove", (e) => {
     }
 });
 
+// 프로그래스 바에서 클릭을 놓았을 때 (클릭한 이후엔 플레이어가 자유롭게 마우스를 움직일 수 있으므로 document에 달아놓음)
 document.addEventListener("pointerup", () => {
     isMove = false;
     song.muted = false;
 });
 
+//플레이리스트 선곡
 playerPlayList.forEach((item, index) => {
 
     item.addEventListener("click", function() {
@@ -254,3 +269,6 @@ playerPlayList.forEach((item, index) => {
     });
     
 });
+
+
+console.log("발견된 문제가 없습니다.");
